ff2ef5cd86657db4747fffbde2663e4de74b511a
ryusun <ryusunshine@gmail.com>
2024-06-11 23:46:21 +0900

    feat:ryusun - 0611 coding study:

--- src/main/java/org/example/그리디/프로그래머스_키패드누르기.java
+++ src/main/java/org/example/그리디/프로그래머스_키패드누르기.java
@@ -9,12 +9,15 @@ public class 프로그래머스_키패드누르기 {
     public String solution(int[] numbers, String hand) {
         String answer = "";
         // 왼손, 오른손 초기값 설정
-        left = new Position(0, 3);
-        right = new Position(2, 3);
+        left = new Position(3, 0);
+        right = new Position(3, 2);
 
         // 다음 눌러야할 숫자 위치 설정
         for (int num : numbers) {
             numPos = new Position((num - 1) / 3, (num - 1) % 3); // row, col
+            if (num == 0) {
+                numPos = new Position(3, 1);
+            }
             String finger = numPos.getFinger(hand); // 다음에 눌러야할 숫자(numPos)에 있어야 할 손가락을 구해(.getFinger) 라고 생각해. 초기값 hand 넣어주기
             answer += finger;
             // 눌러야할 손가락(finger)을 해당 숫자 위치로 이동하기
@@ -45,6 +48,7 @@ public class 프로그래머스_키패드누르기 {
             else if (this.col == 2)
                 finger = "R";
             else {
+                //left, right는 위에서 초기화한 멤버 변수
                 int leftDist = left.getDist(this);
                 int rightDist = right.getDist(this);
 
@@ -52,14 +56,14 @@ public class 프로그래머스_키패드누르기 {
                     finger = "L";
                 else if (leftDist > rightDist)
                     finger = "R";
-                // leftDist == rightDist이면 초기값 right(문제에서 주 손) 으로 return
+                // else 생략(leftDist, rightDist 거리가 같으면) 주 손으로 반환
             }
 
             return finger;
         }
 
-        public int getDist(Position p) {
-            // this(왼손/오른손 위치) , p(눌러야할 숫자 위치)
+        public int getDist(Position p) { // p 는 numPos
+            // 여기서 this 는 left, right
             return Math.abs(this.col - p.col) + Math.abs(this.row - p.row);
         }
     }
